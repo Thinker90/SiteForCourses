a68346f708cd0b53734014282a94064f286acc55
Thinker90 <100183203+Thinker90@users.noreply.github.com>
2023-04-21 15:56:23 +0500

    17412004

--- MySite/main/views.py
+++ MySite/main/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {})
+    return render(request, "index.html", {'activePage': 1})
 
 def blog(request):
-    return render(request, "blog.html", {})
+    return render(request, "blog.html", {'activePage': 4})
 
 def courses(request):
-    return render(request, "courses.html", {})
+    return render(request, "courses.html", {'activePage': 2})
 
 def instructors(request):
-    return render(request, "instructors.html", {})
+    return render(request, "instructors.html", {'activePage': 3})
 
 def contact(request):
-    return render(request, "contact.html", {})
+    return render(request, "contact.html", {'activePage': 5})
 
